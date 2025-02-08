bea88470e38bf4ec20ae9a5eaca4cc5dc060fd81
5upportPark <5upportpark7@gmail.com>
2025-02-08 17:19:02 +0900

    init

---
+++ src/test/java/com/backend/pjw/PjwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
